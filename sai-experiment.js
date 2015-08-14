3f5d52b385c497da85d624818ebf5de1356d078e
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2015-08-14 17:26:43 +0200

    Perfected track player

--- sai-experiment.js
+++ sai-experiment.js
@@ -8,6 +8,22 @@ function midiToFrequency(midiNote) {
     return 440 * Math.pow(Math.pow(2, 1/12), midiNote - 69);
 };
 
+function wait(audioCtx, callback, when) {
+    var test = function() {
+        var delta = when - audioCtx.currentTime;
+        if (delta <= 0) {
+            callback();
+        } else {
+            setTimeout(test, delta * 1000);
+        }
+    };
+    test();
+};
+sai.wait = wait;
+
+/*
+    A note for unique usage.
+*/
 sai.Note = function(audioCtx, instrument, note) {
     this.audioCtx = audioCtx;
     this.instrument = instrument;
@@ -28,7 +44,8 @@ sai.Note = function(audioCtx, instrument, note) {
     
     this.output = this.noteGain;
 };
-sai.Note.prototype.play = function(when) {
+sai.Note.prototype.play = function(when, endCallback) {
+    this.when = when;
     var t  = when;
     var v = this.output.gain;
     v.setValueAtTime(0, t);
@@ -41,6 +58,17 @@ sai.Note.prototype.play = function(when) {
     this.sources.forEach(function(source) {
         source.start();
     }.bind(this));
+    wait(this.audioCtx, function() {
+        this.kill();
+        if (endCallback)
+            endCallback();
+    }.bind(this), t);
+};
+sai.Note.prototype.kill = function() {
+    this.sources.forEach(function(source) {
+        source.stop();
+    }.bind(this));
+    this.output.disconnect();
 };
 
 sai.Track = function(audioCtx, instrument) {
@@ -51,10 +79,57 @@ sai.Track = function(audioCtx, instrument) {
     
     this.output = this.input;
 };
-sai.Track.prototype.playNote = function(note, when) {
+sai.Track.prototype.playNote = function(note, when, endCallback) {
+    when = when || this.audioCtx.currentTime;
     var n = new sai.Note(this.audioCtx, this.instrument, note);
     n.output.connect(this.input);
-    n.play(when);
+    n.play(when, endCallback);
+    return n;
+};
+
+sai.TrackPlayer = function(track, song) {
+    this.track = track;
+    this.song = song;
+    this.state = "stopped";
+    this.job = {};
+};
+sai.TrackPlayer.prototype.playTrack = function(repeat) {
+    if (this.state !== "stopped")
+        return;
+    this.state = "playing";
+    var job = {};
+    this.job = job;
+    var begin = this.track.audioCtx.currentTime;
+    var current = 0;
+    var notes = this.song.notes;
+    var handleNote = function() {
+        if (job.stopping)
+            return;
+        if (current >= notes.length) {
+            if (repeat) {
+                current = 0;
+                begin = begin + this.song.duration;
+            } else
+                return;
+        }
+        var noteTime = begin + notes[current][0];
+        job.lastNote = this.track.playNote(notes[current][1], noteTime);
+        current += 1;
+        wait(this.track.audioCtx, handleNote, noteTime);
+    }.bind(this);
+    handleNote();
+    if (! repeat)
+        wait(this.track.audioCtx, function() {
+            if (job.stopping)
+                return;
+            this.state = "stopped";
+        }.bind(this), ab + this.song.duration);
+};
+sai.TrackPlayer.prototype.stop = function() {
+    if (this.job.lastNote)
+        this.job.lastNote.kill();
+    this.job.stopping = true;
+    this.state = "stopped";
 };
 
 })();
