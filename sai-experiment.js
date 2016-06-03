f39c19b9b7272167f5f28164174d2593ee08dd32
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2016-06-03 13:44:37 +0200

    First working version with new architecture

--- sai-experiment.js
+++ sai-experiment.js
@@ -46,190 +46,257 @@ sai.BaseNode = class BaseNode {
     A note for unique usage.
 */
 sai.Voice = class Voice extends sai.BaseNode {
-    constructor(context, instrument, note) {
+    constructor(context) {
         super(context);
-        this.instrument = instrument;
-        this.voiceGain = context.createGain();
-        this.voiceGain.gain.value = 0;
+        this._note = 69;
+        this.attack = 0.5;
+        this.decay = 0.5;
+        this.sustain = 0.5;
+        this.release = 0.5;
         
-        this.sources = [];
-        instrument.oscillators.forEach(function(oscillator) {
-            var sourceGain = context.createGain();
-            sourceGain.gain.value = oscillator.gain;
-            sourceGain.connect(this.voiceGain);
-            var fOsc, fOscGain = null;
-            if (oscillator.freqOsc) {
-                var fOsc = context.createOscillator();
-                var fOscGain = context.createGain();
-                fOsc.connect(fOscGain);
-                fOsc.type = oscillator.freqOsc.type;
-                fOsc.frequency.value = oscillator.freqOsc.frequency;
-                fOscGain.gain.value = midiToFrequency(note) * oscillator.freqOsc.amount;
-                this.sources.push(fOsc);
-            }
-            if (oscillator.type === "noise") {
-                var whiteNoise = context.createBufferSource();
-                whiteNoise.buffer = noiseBuffer;
-                whiteNoise.loop = true;    
-                var noiseFilter = context.createBiquadFilter();
-                noiseFilter.type = "bandpass";
-                noiseFilter.frequency.value = midiToFrequency(note);
-                whiteNoise.connect(noiseFilter);
-                // compensation of gain to make it more audible on low notes
-                sourceGain.gain.value *= 1 + (( -(note - 69)) / (12 * 4));
-                noiseFilter.connect(sourceGain);
-                if (fOsc)
-                    fOscGain.connect(noiseFilter.frequency);
-                this.sources.push(whiteNoise);
-            } else {
-                var source = context.createOscillator();
-                source.type = oscillator.type;
-                source.frequency.value = midiToFrequency(note);
-                source.connect(sourceGain);
-                if (fOsc)
-                    fOscGain.connect(source.frequency);
-                this.sources.push(source);
-            }
-        }.bind(this));
+        // oscillator 1
+        this.osc1 = new sai.Oscillator(context);
         
-        this.output = this.voiceGain;
+        // oscillator 2
+        this.osc2 = new sai.Oscillator(context);
+        
+        // oscillators mixer
+        this.oscMixer = context.createGain();
+        this.osc1.connect(this.oscMixer);
+        this.osc2.connect(this.oscMixer);
+        
+        // filter
+        //this.filter = context.createBiquadFilter();
+        //this.oscMixer.connect(this.filter);
+        
+        // envelope gain
+        this.envelope = context.createGain();
+        //this.filter.connect(this.envelope);
+        this.oscMixer.connect(this.envelope);
+        
+        // main gain
+        this.output = context.createGain();
+        this.envelope.connect(this.output);
+        
+        // lfo
+        /*
+        this.lfo = new sai.Oscillator(context);
+        
+        this.lfoOsc1Gain = context.createGain();
+        this.lfoOsc1Gain.gain.value = 0;
+        this.lfo.connect(this.lfoOsc1Gain);
+        this.lfoOsc1Gain.connect(this.osc1.frequencyParam);
+        
+        this.lfoOsc2Gain = context.createGain();
+        this.lfoOsc2Gain.gain.value = 0;
+        this.lfo.connect(this.lfoOsc2Gain);
+        this.lfoOsc2Gain.connect(this.osc2.frequencyParam);
+        
+        this.lfoFilterGain = context.createGain();
+        this.lfoFilterGain.gain.value = 0;
+        this.lfo.connect(this.lfoFilterGain);
+        this.lfoFilterGain.connect(this.filter.frequency);
+        */
     }
-    play(when, setEnd, endCallback) {
+    start(when) {
         when = when || this.context.currentTime;
-        setEnd = setEnd === undefined ? true : setEnd;
-        this.when = when;
+        this.startTime = when;
         var t  = when;
-        var v = this.output.gain;
-        v.setValueAtTime(0, t);
-        t += this.instrument.attack;
-        v.linearRampToValueAtTime(1, t);
-        t += this.instrument.decay;
-        v.linearRampToValueAtTime(this.instrument.sustainLevel, t);
-        this.decayEnd = t;
-        this.endCallback = endCallback;
-        if (setEnd) {
-            this.end(t + this.instrument.sustainTime);
-        }
-        this.sources.forEach(function(source) {
-            source.start();
-        }.bind(this));
+        this.envelope.gain.setValueAtTime(0, t);
+        t += this.attack;
+        this.envelope.gain.linearRampToValueAtTime(1, t);
+        t += this.decay;
+        this.envelope.gain.linearRampToValueAtTime(this.sustain, t);
+        this.osc1.start(when);
+        this.osc2.start(when);
+        //this.lfo.start(when);
     }
-    end(when) {
-        if (this.ended)
-            return;
-        this.ended = true;
+    stop(when) {
         when = when || this.context.currentTime;
-        when = Math.max(when, this.decayEnd);
         var t = when;
-        var v = this.output.gain;
-        v.setValueAtTime(this.instrument.sustainLevel, t);
-        t += this.instrument.release;
-        v.linearRampToValueAtTime(0, t);
-        wait(this.context, function() {
-            this.kill();
-            if (this.endCallback)
-                this.endCallback();
-        }.bind(this), t);
+        this.envelope.gain.cancelScheduledValues(t);
+        t += this.release;
+        this.envelope.gain.linearRampToValueAtTime(0, t);
+        this.osc1.stop(t);
+        this.osc2.stop(t);
+        //this.lfo.stop(t);
     }
-    kill() {
-        this.sources.forEach(function(source) {
-            source.stop();
-        }.bind(this));
-        this.output.disconnect();
+    get note() {
+        return this._note;
+    }
+    set note(val) {
+        this._note = val;
+        this.osc1.frequency.value = midiToFrequency(val);
+        this.osc2.frequency.value = midiToFrequency(val);
+    }
+    get osc1Type() {
+        return this.osc1.type;
+    }
+    set osc1Type(val) {
+        this.osc1.type = val;
+    }
+    get osc1Gain() {
+        return this.osc1.gain;
+    }
+    get osc2Type() {
+        return this.osc2.type;
+    }
+    set osc2Type(val) {
+        this.osc2.type = val;
+    }
+    get osc2Gain() {
+        return this.osc2.gain;
     }
 }
 
 sai.Track = class Track extends sai.BaseNode {
-    constructor(context, instrument) {
-        super(context);
-        
-        this._input = context.createGain();
-        
-        var nodes = [this._input];
-        instrument.filters.forEach(function(filter) {
-            var node = context.createBiquadFilter();
-            node.type = filter.type;
-            nodes[nodes.length - 1].connect(node);
-            nodes.push(node);
-        });
-        this.lastFilter = nodes[nodes.length - 1];
-        this.filters = nodes.slice(1);
-        
-        this.pannerNode = this.context.createStereoPanner();
-        this.lastFilter.connect(this.pannerNode);
-        this.panOsc = this.context.createOscillator();
-        this.panOscGain = this.context.createGain();
-        this.panOsc.connect(this.panOscGain);
-        this.panOscGain.connect(this.pannerNode.pan);
-        this.panOsc.start();
-        
-        this.volumeGain = this.context.createGain();
-        this.pannerNode.connect(this.volumeGain);
-        
-        this.delay = new sai.Delay(this.context);
-        this.volumeGain.connect(this.delay.input);
-        
-        this.output = this.context.createGain();
-        this.delay.connect(this.output);
-
-        this.setInstrument(instrument);
-    }
-    setInstrument(instrument) {
-        this.instrument = instrument;
-        for (var i = 0; i < this.filters.length && i < instrument.filters.length; i++) {
-            if (i >= instrument.filters.length)
-                return;
-            this.filters[i].frequency.value = instrument.filters[i].frequency;
-            this.filters[i].gain.value = instrument.filters[i].gain;
-            this.filters[i].Q.value = instrument.filters[i].q;
-        }
-        this.panOsc.frequency.value = instrument.panFrequency;
-        this.panOscGain.gain.value = instrument.panAmount;
-        this.volumeGain.gain.value = instrument.gain;
-        this.delay.gain = instrument.delay;
-        this.delay.delayTime = instrument.delayTime;
-    }
-    playNote(note, when, setEnd, endCallback) {
-        when = when || this.context.currentTime;
-        setEnd = setEnd === undefined ? true : setEnd;
-        var n = new sai.Voice(this.context, this.instrument, note);
-        n.connect(this._input);
-        n.play(when, setEnd, endCallback);
-        return n;
-    }
-    kill() {
-        this.panOsc.stop();
-    }
-}
-
-sai.Delay = class Delay extends sai.BaseNode {
     constructor(context) {
         super(context);
-        this.input = this.context.createGain();
         
-        this.delayGain = this.context.createGain();
-        this.input.connect(this.delayGain);
-
-        this.delay = this.context.createDelay();
-        this.delayGain.connect(this.delay);
-        this.delay.connect(this.delayGain);
+        this._mixer = context.createGain();
+        
+        this._voices = {};
         
         this.output = this.context.createGain();
-        this.delay.connect(this.output);
-        this.input.connect(this.output);
+        this._mixer.connect(this.output);
+        
+        this._voices = {};
+        
+        this.osc1Type = "sine";
+        this.osc1Gain = 1;
+        this.osc2Type = "sine";
+        this.osc2Gain = 1;
+        this.gain = 1;
+    }
+    midiMessage(message) {
+        if (message.cmd === sai.MidiMessage.commands.noteOn) {
+            if (this._voices[message.note])
+                return;
+            var voice = new sai.Voice(this.context);
+            this._voices[message.note] = voice;
+            voice.note = message.note;
+            voice.osc1Type = this.osc1Type;
+            voice.osc2Type = this.osc2Type;
+            voice.osc1Gain.value = this.osc1Gain;
+            voice.osc2Gain.value = this.osc2Gain;
+            voice.connect(this._mixer);
+            voice.start();
+        } else if (message.cmd === sai.MidiMessage.commands.noteOff) {
+            if (! this._voices[message.note])
+                return;
+            this._voices[message.note].stop();
+            delete this._voices[message.note];
+        }
+    }
+    get osc1Type() {
+        return this._osc1Type;
+    }
+    set osc1Type(val) {
+        this._osc1Type = val;
+        _.each(this._voices, (v) => v.osc1Type = val);
+    }
+    get osc1Gain() {
+        return this._osc1Gain;
+    }
+    set osc1Gain(val) {
+        this._osc1Gain = val;
+        _.each(this._voices, (v) => v.osc1Gain = val);
+    }
+    get osc2Type() {
+        return this._osc2Type;
+    }
+    set osc2Type(val) {
+        this._osc2Type = val;
+        _.each(this._voices, (v) => v.osc2Type = val);
+    }
+    get osc2Gain() {
+        return this._osc2Gain;
+    }
+    set osc2Gain(val) {
+        this._osc2Gain = val;
+        _.each(this._voices, (v) => v.osc2Gain = val);
     }
     get gain() {
-        return this.delayGain.gain.value;
+        return this.output.gain.value;
     }
     set gain(val) {
-        this.delayGain.gain.value = val;
-    }
-    get delayTime() {
-        return this.delay.delayTime.value;
-    }
-    set delayTime(val) {
-        this.delay.delayTime.value = val;
+        this.output.gain.value = val;
     }
 }
+
+sai.Oscillator = class Oscillator extends sai.BaseNode {
+    constructor(context) {
+        super(context);
+        
+        this._type = "sine";
+        
+        this.osc = context.createOscillator();
+        this.oscOutput = context.createGain();
+        this.osc.connect(this.oscOutput);
+        
+        this.whiteNoise = context.createBufferSource();
+        this.whiteNoise.buffer = noiseBuffer;
+        this.whiteNoise.loop = true;
+        this.whiteNoiseOutput = context.createGain();
+        this.whiteNoiseOutput.gain.value = 0;
+        this.whiteNoise.connect(this.whiteNoiseOutput);
+        
+        this.output = context.createGain();
+        this.oscOutput.connect(this.output);
+        this.whiteNoiseOutput.connect(this.output);
+    }
+    start(when) {
+        this.osc.start(when);
+        this.whiteNoise.start(when);
+    }
+    stop(when) {
+        this.osc.stop(when);
+        this.whiteNoise.stop(when);
+    }
+    get type() {
+        return this._type;
+    }
+    set type(val) {
+        this._type = val;
+        if (this._type === "noise") {
+            this.whiteNoiseOutput.gain.value = 1;
+            this.oscOutput.gain.value = 0;
+            this.osc.type = "sine";
+        } else {
+            this.whiteNoiseOutput.gain.value = 0;
+            this.oscOutput.gain.value = 1;
+            this.osc.type = this._type;
+        }
+    }
+    get frequency() {
+        return this.osc.frequency;
+    }
+    get gain() {
+        return this.output.gain;
+    }
+}
+
+sai.MidiMessage = class MidiMessage {
+    constructor(data) {
+        this.cmd = data ? data[0] >> 4 : 0;
+        this.channel = data ? data[0] & 0xf : 0;
+        this.type = data ? data[0] & 0xf0 : 0;
+        this.note = data ? data[1] : 0;
+        this.velocity = data ? data[2] : 0;
+    }
+    get cmdString() {
+        return _.findKey(sai.MidiMessage.commands, (x) => x === this.cmd);
+    }
+}
+
+sai.MidiMessage.commands = {
+    noteOff: 0x8,
+    noteOn: 0x9,
+    polyphonicKeyPressure: 0xA,
+    controlChange: 0xB,
+    programChange: 0xC,
+    channelPressure: 0xD,
+    pitchBendChange: 0xE,
+};
 
 })();
