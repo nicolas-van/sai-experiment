01b23b9e836d53c7e1fbf05e49d2d8d317406f86
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2020-09-01 11:10:10 +0200

    Added libraries for the demo app

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,4 +1,14 @@
 
+// import some polyfill to ensure everything works OK
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
+// import bootstrap's javascript part
+import 'bootstrap'
+
+// import font awesome fonts
+import '@fortawesome/fontawesome-free/css/all.css'
+
 import * as sai from '../src/sai-experiment'
 
 console.log('loading track')
